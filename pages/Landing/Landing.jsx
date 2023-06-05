c292270aa2de011be8d45842336ac0db1be46f04
ayoniyi <ayo.a.niyi@gmail.com>
2023-06-05 14:16:19 -0700

    update: sections 2:15

--- pages/Landing/Landing.jsx
+++ pages/Landing/Landing.jsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import style from "./Landing.module.scss";
-import { circle1, circle2, polygon1, iphone } from "./assets";
+import {
+  circle1,
+  circle2,
+  circle3,
+  polygon1,
+  iphone,
+  logo1,
+  logo2,
+  logo3,
+  look1,
+  look2,
+  settings1,
+  door1,
+  stock,
+  bank,
+  crypto,
+  cards,
+} from "./assets";
 import Header from "../../components/header/Header";
 import Cursor from "../../components/cursorFire/Cursor";
 import Intro from "../../components/intro/Intro";
@@ -46,6 +63,135 @@ const Landing = () => {
 
         <div className={style.circle2}>
           <Image src={circle2} alt="circle" />
+        </div>
+      </section>
+      <section className={style.builtSection}>
+        <div className={style.builtContent}>
+          <h3>
+            Built from the ground up so you <span> never </span> have to go
+            through the <span>stress</span>.
+          </h3>
+          <p>
+            Stack was built by ex senior software engineers from top fintechs
+            around the world so that you can focus on running your business and
+            not worry about building.
+          </p>
+          <div className={style.builtLogos}>
+            <Image src={logo1} alt="logo1" />
+            <Image src={logo2} alt="logo2" />
+            <Image src={logo3} alt="logo3" />
+          </div>
+        </div>
+      </section>
+      <section className={style.helpSection}>
+        <div className={style.circle3}>
+          <Image src={circle3} alt="circle" />
+        </div>
+        <div className={style.helpContent}>
+          <div className={style.helpText}>
+            <h4>
+              Who We <span>Help</span>{" "}
+            </h4>
+            <h3>Built with your startup idea in mind.</h3>
+            <p>
+              Stack was built to cover many use-cases in fintech. Plug into our
+              API or use our pre-built white-labeled solutions to get your
+              startup off the ground in weeks!
+            </p>
+          </div>
+          <div className={style.boxes}>
+            <div className={style.boxRows}>
+              <div className={style.box}>
+                <Image src={look1} alt="look" />
+                <h3>Exploration</h3>
+                <p>
+                  You just are exploring a new startup idea and trying to decide
+                  what partner is best for you.
+                </p>
+              </div>
+              <div className={style.box}>
+                <Image src={look2} alt="look" />
+                <h3>New product offering</h3>
+                <p>
+                  You just are exploring a new startup idea and trying to decide
+                  what partner is best for you.
+                </p>
+              </div>
+            </div>
+            <div className={style.boxRows}>
+              <div className={style.box}>
+                <Image src={settings1} alt="look" />
+                <h3>Idea validation</h3>
+                <p>
+                  You are trying to validate a business idea you have. You are
+                  probably about to start building your MVP for a small
+                  audience.
+                </p>
+              </div>
+              <div className={style.box}>
+                <Image src={door1} alt="look" />
+                <h3>Scaling</h3>
+                <p>
+                  You are looking to scale your fintech startup, add new product
+                  offerings and enter into new international markets.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={style.products}>
+        <div className={style.productsContent}>
+          <div className={style.productTxt}>
+            <h3>
+              Some Of Our <span>Products</span>{" "}
+            </h3>
+            <h2>Build the startup your customers deserve.</h2>
+          </div>
+          <div className={style.productBoxes}>
+            <div className={style.productBoxBig}>
+              <div className={style.pBBTxt}>
+                <h3>Build banking apps</h3>
+                <p>Straight from the federal reserve to your application.</p>
+                <button>Learn more</button>
+              </div>
+              <div className={style.pBBImg}>
+                <Image src={bank} alt="banking" />
+              </div>
+            </div>
+            <div className={style.productBoxRow}>
+              <div className={style.productBoxSmall}>
+                <div className={style.pBSTxt}>
+                  <h3>Build crypto apps</h3>
+                  <p>Straight from the blockchain to your application</p>
+                  <button>Learn more</button>
+                </div>
+                <div className={style.pBSImg}>
+                  <Image src={crypto} alt="blockchain" />
+                </div>
+              </div>
+              <div className={style.productBoxSmall}>
+                <div className={style.pBSTxt}>
+                  <h3>Build stock trading apps</h3>
+                  <p>Straight from the stock market, to your application</p>
+                  <button>Learn more</button>
+                </div>
+                <div className={style.pBSImg}>
+                  <Image src={stock} alt="stock market" />
+                </div>
+              </div>
+            </div>
+            <div className={style.productBoxBig}>
+              <div className={style.pBBTxt}>
+                <h3>Embed card issuing</h3>
+                <p>Issue virtual and physical cards to your users.</p>
+                <button>Learn more</button>
+              </div>
+              <div className={style.pBBImg}>
+                <Image src={cards} alt="virtual cards" />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
--- pages/Landing/assets/index.js
+++ pages/Landing/assets/index.js
@@ -1,6 +1,35 @@
 import circle1 from "./ellipse1.svg";
 import circle2 from "./ellipse2.svg";
+import circle3 from "./ellipse3.svg";
 import iphone from "./iphone11.png";
 import polygon1 from "./polygon1.svg";
+import logo1 from "./contrologo.svg";
+import logo2 from "./maplelogo.svg";
+import logo3 from "./verifiedlylogo.svg";
+import look1 from "./binocular1.svg";
+import look2 from "./binocular2.svg";
+import settings1 from "./settings1.svg";
+import door1 from "./door.svg";
+import bank from "./bank.svg";
+import crypto from "./crypto.svg";
+import stock from "./stock.svg";
+import cards from "./cards.svg";
 
-export { circle1, circle2, iphone, polygon1 };
+export {
+  circle1,
+  circle2,
+  circle3,
+  iphone,
+  polygon1,
+  logo1,
+  logo2,
+  logo3,
+  look1,
+  look2,
+  settings1,
+  door1,
+  bank,
+  crypto,
+  stock,
+  cards,
+};
